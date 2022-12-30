d5f96185752dfaaa11cb5b52d5b3ae9de1f6356b
Luis Rivera <12088563+jarnosz@users.noreply.github.com>
2022-12-30 13:08:06 -0600

    replaced renamed files, per TeXLive request

---
+++ luatex/blot-sets.lua
@@ -0,0 +1,19 @@
+return {
+  
+  features  = {
+    kern    = true,
+    liga    = true,
+    trep    = true,
+    tlig    = true,
+    script  = "latn",
+    lang    = "dflt",
+    --stretch = 30,
+    --shrink  = 20,
+    --step    = 10,
+  },
+
+  normal = {
+    "Normal", "Regular", "Book",
+  },
+  
+}
